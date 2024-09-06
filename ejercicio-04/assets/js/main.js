437fb4736a67ca95a842e8663c9e51feab926749
guillermo1723 <00044123@uca.edu.sv>
2024-09-05 21:57:01 -0600

    Se ha terminado el ejercicio 4 y funciona

---
+++ ejercicio-04/assets/js/main.js
@@ -0,0 +1,35 @@
+const createCounter = () => {
+    let contador = 0;
+    const newContador = () => {
+        contador++;
+        return contador;
+    };
+    return newContador;
+}
+
+const showCounter = (counterFunction, number) => {
+    for (let i = 0; i < number; i++) {
+        alert("Valor: " + counterFunction());
+    }
+}
+
+const requestNumber=()=>{
+    let number = parseInt(prompt("Ingrese un número no negativo hasta el cual  deseas contar"));
+    if (isNaN(number) || number < 0) {
+        alert("El número que ha ingresado no es valido");
+        return null;
+    } else {
+        alert("El número que ha ingresado es valido");
+        return number;
+    }
+}
+
+const main = () => {
+    let number = requestNumber ();
+    if (number !== null) {
+        const counterFunction = createCounter(); 
+        showCounter(counterFunction, number);
+    }
+}
+
+main();
