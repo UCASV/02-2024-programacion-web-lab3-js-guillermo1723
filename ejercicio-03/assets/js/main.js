989e85d8fb19f840ce91cd12163f1cd2ac291efb
guillermo1723 <00044123@uca.edu.sv>
2024-09-05 20:56:16 -0600

    Se han corregido errores del ejercicio 3

--- ejercicio-03/assets/js/main.js
+++ ejercicio-03/assets/js/main.js
@@ -1,20 +1,27 @@
-const calculateFactorial = (/*recibe*/) => {
-    //Code
+const calculateFactorial = (number) => {
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+    alert(`El valor del factorial que ha ingresado es ${factorial}`);
 }
 
 const requestNumber = () => {
-    let number;
-    alert ("Ingrese un numero para calcular el factorial, no negativo")
-    prompt ("Ingrese el valor");
-    if (number < 0) {
-        alert ("El numero que ha ingresado es no valido")
-    }else {
-        console.log ("El valor ingresado es valido")
+    let number = parseInt(prompt("Ingrese un número para calcular el factorial, no negativo"));
+    if (isNaN(number) || number < 0) {
+        alert("El número que ha ingresado no es valido");
+        return null;
+    } else {
+        alert("El número que ha ingresado es valido");
+        return number;
     }
 }
 
 const main = () => {
-    requestNumber ();
+    let number = requestNumber(); 
+    if (number !== null) {  
+        calculateFactorial(number);
+    }
 }
 
 main();
